b2395edd4c21cb027b2f600c38ae4eee8999f4e2
Matthew Knudsen <mknudsen01@gmail.com>
2014-09-07 12:49:06 -0700

    console log game working --start, new turns, game over

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,77 @@
 var Simon = Simon || {};
 
 Simon.Controller = function(opts){
+  var self = this;
   this.game = opts.game;
+  this.view = opts.view;
+
+  this.executeTurn = function() {
+    console.log("NEW TURN");
+    // turn off listeners for clicks?
+    // tell game to start turn
+    self.game.nextTurn();
+    // tell view to show the sequence, taking the game as an argument?
+    self.view.showSequence(self.game);
+    // turn listeners on for the clicks/have some checker for a click.
+  };
+
+  this.bindListeners = function(){
+    document.querySelector('.board').addEventListener('click', this.buttonClicked);
+    document.addEventListener('correct', this.correctGuess);
+    document.addEventListener('wrong', this.wrongGuess);
+    document.addEventListener('newTurn', this.executeTurn);
+  };
+
+  this.buttonClicked = function(e){
+    var clickedButton = parseInt(e.target.dataset.gameButton);
+    self.game.checkGuess(clickedButton);
+  };
+
+  this.correctGuess = function(){
+    console.log("YOU GUESSED CORRECTLY");
+  };
+
+  this.wrongGuess = function(){
+    console.log("YOU GUESSED WRONG");
+    self.restartGame();
+  };
+
+  this.restartGame = function(){
+    self.game.restart();
+    self.view.restart();
+    self.executeTurn();
+  }
+
+
 };
+
+// Simon.Controller.prototype = {
+//   executeTurn: function() {
+//     // turn off listeners for clicks?
+//     // tell game to start turn
+//     this.game.startTurn();
+//     // tell view to show the sequence, taking the game as an argument?
+//     this.view.showSequence(this.game);
+//     // turn listeners on for the clicks/have some checker for a click.
+//   },
+
+//   bindListeners: function(){
+//     document.querySelector('.board').addEventListener('click', this.buttonClicked.bind(this));
+//     document.addEventListener('correct', this.correctGuess.bind(this));
+//     document.addEventListener('wrong', this.wrongGuess.bind(this));
+//   },
+
+//   buttonClicked: function(e){
+//     var clickedButton = parseInt(e.target.dataset.gameButton);
+//     this.game.checkGuess(clickedButton);
+//   },
+
+//   wrongGuess: function(){
+//     console.log("YOU GUESSED WRONG");
+//   },
+
+//   correctGuess: function(){
+//     console.log("YOU GUESSED CORRECTLY");
+//     this.game.
+//   }
+// };
--- js/game.js
+++ js/game.js
@@ -1,5 +1,55 @@
 var Simon = Simon || {};
 
 Simon.Game = function(){
-  this.score = 0;
+  this.totalScore = 0;
+  this.turnScore = 0;
+  this.sequence = [];
+};
+
+Simon.Game.prototype = {
+  nextTurn: function() {
+    this.turnScore = 0;
+    this.sequence.push(this.randomNumberBetweenOneAnd(4));
+  },
+
+  randomNumberBetweenOneAnd: function(n) {
+    return Math.floor(Math.random() * n) + 1;
+  },
+
+  checkGuess: function(guess){
+    var currentPosition = this.turnScore;
+    nextCorrectGuess = this.sequence[currentPosition];
+    if (guess == nextCorrectGuess){
+     this.correctGuess();
+    } else {
+      this.wrongGuess();
+    }
+  },
+
+  correctGuess: function(){
+    this.totalScore += 1;
+    this.turnScore += 1;
+    document.dispatchEvent( new Event("correct") );
+    if( this.isTurnFinished() ){
+      document.dispatchEvent( new Event("newTurn") );
+    }
+  },
+
+  wrongGuess: function(){
+    document.dispatchEvent( new Event("wrong") );
+  },
+
+  restart: function(){
+    this.totalScore = 0;
+    this.turnScore = 0;
+    this.sequence = [];
+  },
+
+  isTurnFinished: function() {
+    return this.turnScore == this.sequence.length;
+  }
+
+
+
+
 };
